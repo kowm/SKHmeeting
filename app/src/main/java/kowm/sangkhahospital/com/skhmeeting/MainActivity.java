2b47c79685b45d6719f2bae40dee110d8b7a3b41
kowm <mahanam277@gmail.com>
2018-06-27 16:24:10 +0700

    skh 0.1

--- app/src/main/java/kowm/sangkhahospital/com/skhmeeting/MainActivity.java
+++ app/src/main/java/kowm/sangkhahospital/com/skhmeeting/MainActivity.java
@@ -1,7 +1,10 @@
 package kowm.sangkhahospital.com.skhmeeting;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import kowm.sangkhahospital.com.skhmeeting.fragment.Mainflagment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+   //     Add fragment to Activity
+        if (savedInstanceState == null){
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentmainflagment,new Mainflagment())
+                    .commit();
+        }
+
+    } //main Method
+
+}  //main Class
