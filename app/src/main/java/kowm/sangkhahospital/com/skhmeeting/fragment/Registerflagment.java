8592f951ac85ab5361a2a056b5a3a587d95fba10
kowm <mahanam277@gmail.com>
2018-06-29 12:07:59 +0700

    skh 0.7

--- app/src/main/java/kowm/sangkhahospital/com/skhmeeting/fragment/Registerflagment.java
+++ app/src/main/java/kowm/sangkhahospital/com/skhmeeting/fragment/Registerflagment.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,10 +13,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import it.sauronsoftware.ftp4j.FTPDataTransferListener;
 import kowm.sangkhahospital.com.skhmeeting.MainActivity;
 import kowm.sangkhahospital.com.skhmeeting.R;
 
@@ -23,6 +28,7 @@ public class Registerflagment extends Fragment{
 
     private ImageView imageView;
     private Uri uri;
+    private boolean aBoolean = true;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -47,10 +53,86 @@ public class Registerflagment extends Fragment{
     }   //        Main Method
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itmeUpload) {
+
+            uploadValueToServer();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadValueToServer() {
+        EditText nameEditText = getView().findViewById(R.id.editname);
+        EditText userEditText = getView().findViewById(R.id.editUser);
+        EditText passwordEditText = getView().findViewById(R.id.editpassword);
+
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+
+        if (aBoolean) {
+            alertMessage("Non Choose Avata");
+        } else if (nameString.isEmpty() || userString.isEmpty()|| passwordString.isEmpty()) {
+            alertMessage("Please Fill All Blank");
+        } else {
+
+            // upload Image Avata
+
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy
+                    .Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+
+        }
+
+
+    }   // upload
+
+    public class MyUploadAvata implements FTPDataTransferListener{
+        @Override
+        public void started() {
+            alertMessage("Start Upload Avata");
+        }
+
+        @Override
+        public void transferred(int i) {
+            alertMessage("Continue Upload Avata ");
+        }
+
+        @Override
+        public void completed() {
+            alertMessage("Success Upload Avata");
+        }
+
+        @Override
+        public void aborted() {
+
+        }
+
+        @Override
+        public void failed() {
+
+        }
+    }
+
+
+
+
+    private void alertMessage(String strMessage) {
+
+        Toast.makeText(getActivity(), strMessage, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
 
-
+        inflater.inflate(R.menu.menu_register, menu);
     }
 
     @Override
@@ -60,6 +142,8 @@ public class Registerflagment extends Fragment{
         if (resultCode == getActivity().RESULT_OK) {
 
             uri = data.getData();
+            aBoolean = false;
+
             try {
                 Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
 
